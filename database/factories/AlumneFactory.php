b92cdc741ca89a3cb2b897ed6a188086f6afe33f
Anton Dalmau Mines <anton@MacBook-Pro-de-Anton.local>
2021-10-13 11:18:52 +0200

    code

---
+++ database/factories/AlumneFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Alumne;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AlumneFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Alumne::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            "nom" => $this->faker->firstName,
+            "cognoms" => $this->faker->lastName . " " . $this->faker->lastName, 
+            "data_naixement" => $this->faker->date($format = 'Y-m-d', $max = 'now')
+        ];
+    }
+}
